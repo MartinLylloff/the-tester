09a3c20af3fe8351535f9f57ede9c3b55896497a
Martin Lylloff <mart0c95@edu.zealand.dk>
2024-02-07 16:44:52 +0100

    TESTET

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>This is a test</p>
+        <p>
+          This is a <p>TEST</p>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
